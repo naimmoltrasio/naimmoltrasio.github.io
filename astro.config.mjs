c5215356c75322e67f4af141deb93161c89162fe
Naim Moltrasio <naim.moltrasio@gmail.com>
2024-06-12 14:54:18 +0200

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://naimmoltrasio.github.io',
+  base: 'my-repo',
 });
+
